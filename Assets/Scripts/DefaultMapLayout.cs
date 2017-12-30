4c48302c242040058e102d28b7b428f911abc13a
JSB <kyle-naber@uiowa.edu>
2017-12-29 22:22:03 -0600

    more textures. Updated default map values

--- Assets/Scripts/DefaultMapLayout.cs
+++ Assets/Scripts/DefaultMapLayout.cs
@@ -1607,7 +1607,7 @@ public class DefaultMapLayout : MapLayout {
 		};
 		// (16, 6)
 		arrayOfHexes[x, y++] = new Dictionary<hexType, bool> () {
-			{hexType.CITY, false}, {hexType.RAD, false}, {hexType.MOUNTAIN, false}, {hexType.PLAINS, true}, {hexType.WATER, false}, 
+			{hexType.CITY, false}, {hexType.RAD, true}, {hexType.MOUNTAIN, false}, {hexType.PLAINS, true}, {hexType.WATER, false}, 
 			{hexType.BASE, false}, {hexType.RAND_LOC, false}, {hexType.RESOURCE, false}, {hexType.VALID, true}
 		};
 		// (16, 7)
