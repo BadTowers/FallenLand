8225c89069e23fb49421ffa36daa296d00f91875
JSB <kyle-naber@uiowa.edu>
2018-01-03 12:49:31 -0600

    Pressing new game now loads up the new scene correctly.

---
+++ Assets/Scripts/MainMenuScripts/UIManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+	public void asyncSceneLoad(string name){
+		SceneManager.LoadSceneAsync(name);
+	}
+}
