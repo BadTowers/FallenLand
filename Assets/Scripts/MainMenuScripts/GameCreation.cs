199de24d430077f5c7e220482079fcd0e140d8f2
JSB <kyle-naber@uiowa.edu>
2018-01-09 12:33:53 -0600

    Adding more components to the set up game screen

--- Assets/Scripts/MainMenuScripts/GameCreation.cs
+++ Assets/Scripts/MainMenuScripts/GameCreation.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class GameCreation : MonoBehaviour {
 
-	public enum GameType {NormalGame, DemoGame, ExpertGame, ShortGame, TimedGame, VeteranGame, SoloI,
-							SoloIIStandard, SoloIIModerate, SoloIIDifficult, SoloIIUltimate};
+	public enum GameType {NormalGame, SoloI, SoloII};
 	public enum GameModifiers {ProjectGodsHammer, Ronin, HarshReality, TownTechUpgradesCountDouble, StartWithFewerCards, 
-								DemoGamePurchase, DemoGameTradeBeforeGame};
+								DemoGamePurchase, DemoGameTradeBeforeGame, Timed, Expert, Short, Veteran, Demo};
+	public enum SoloII {SoloIIStandard, SoloIIModerate, SoloIIDifficult, SoloIIUltimate};
 
 	public GameType gameMode;
 	public ArrayList allModifiers;
@@ -24,6 +24,34 @@ public class GameCreation : MonoBehaviour {
 			DontDestroyOnLoad (this.gameObject);
 		} else {
 			Destroy (this.gameObject);
+		}
+	}
+
+	public static string getGameTypeName(GameType gt) {
+		switch (gt) {
+		case GameType.NormalGame:
+			return "Normal Game\n";
+		case GameType.SoloI:
+			return "Solo Variant I";
+		case GameType.SoloII:
+			return "Solo Variant II";
+		default:
+			return "NULL";
+		}
+	}
+
+	public static string getSoloIITypeName(SoloII st) {
+		switch (st) {
+		case SoloII.SoloIIStandard:
+			return "Standard--1 Enemy";
+		case SoloII.SoloIIModerate:
+			return "Moderate--2 Enemies";
+		case SoloII.SoloIIDifficult:
+			return "Difficult--3 Enemies";
+		case SoloII.SoloIIUltimate:
+			return "Ultimate--4 Enemies";
+		default:
+			return "NULL";
 		}
 	}
 }
