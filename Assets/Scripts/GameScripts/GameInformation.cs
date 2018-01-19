bd8aa2caf57911c2b8dc7a6a4138aa4ea876dd77
JSB <kyle-naber@uiowa.edu>
2018-01-19 07:50:52 -0600

    Added in GameInformation enum class to start holding enum valuesa for various stats in the game. Added in the default game file to hold all values for default game.

---
+++ Assets/Scripts/GameScripts/GameInformation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInformation : MonoBehaviour {
+
+	public enum GameValues{
+		Starting_Town_Health,
+		Starting_Prestige,
+		Starting_Salvage,
+		Win_Town_Health,
+		Win_Prestige,
+	}
+
+	public enum WinConditions{
+		Town_Health,
+		Prestige,
+		Both,
+	}
+
+	public static int getStartingValue(GameValues stat){
+		switch (stat) {
+		case GameValues.Starting_Salvage:
+			return 10;
+		case GameValues.Starting_Prestige:
+			return 1;
+		case GameValues.Starting_Town_Health:
+			return 30;
+		case GameValues.Win_Prestige:
+			return 20;
+		case GameValues.Win_Town_Health:
+			return 80;
+		}
+	}
+
+}
