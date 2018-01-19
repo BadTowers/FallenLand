e2171bb94eb01a4581fe7558a2df636c41d5514e
JSB <kyle-naber@uiowa.edu>
2018-01-19 08:01:58 -0600

    Added some default values to the default game file.

--- Assets/Scripts/GameScripts/DefaultGame.cs
+++ Assets/Scripts/GameScripts/DefaultGame.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class DefaultGame {
 
-
+	public Dictionary<GameInformation.GameValues, int> defaultValues = new Dictionary<GameInformation.GameValues, int>{
+		{GameInformation.GameValues.Starting_Town_Health, 30},
+		{GameInformation.GameValues.Starting_Prestige, 1},
+		{GameInformation.GameValues.Starting_Salvage, 10},
+		{GameInformation.GameValues.Win_Prestige, 20},
+		{GameInformation.GameValues.Win_Town_Health, 80}
+	};
 }
