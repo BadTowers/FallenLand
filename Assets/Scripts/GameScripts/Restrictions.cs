57351673e7545d910116bdfbffaf9807f4536794
JSB <kyle-naber@uiowa.edu>
2018-01-21 01:46:39 -0600

    Added more getters and setters for card actives and passive

---
+++ Assets/Scripts/GameScripts/Restrictions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public enum Restrictions  {
+
+	//Vehicle
+	Four_Wheeled_Only,
+	Not_Bicycles,
+	Not_Horses,
+
+
+}
--- Assets/Scripts/GameScripts/SpoilsCard.cs
+++ Assets/Scripts/GameScripts/SpoilsCard.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class SpoilsCard : PartyCard {
 
 	private List<SpoilsTypes> types; //The list of all spoils types the card qualifies as
 	private int sellValue; //The value of the card when sold to the bank
 	private int carryWeight; //The weight of the item
-	//specialAbilities. Psych resistance etc TODO
-	//uniqueAbilities. Once per game, do X Y Z. etc TODO
+	private List<List<Gains>> activeGains; //TODO getters and setters
+	private List<Gains> passiveGains;
+	private List<Restrictions> restrictions;
+	private List<List<Times>> whenUsable; //TODO getters and setters
+	private Uses uses; 
 
 	public SpoilsCard(string title) : base(title) {
-
+		types = new List<SpoilsTypes>();
+		activeGains = new List<List<Gains>>();
+		passiveGains = new List<Gains>();
+		restrictions = new List<Restrictions>();
+		whenUsable = new List<List<Times>>();
 	}
 
 	public SpoilsCard(string title, List<SpoilsTypes> types) : base(title) {
@@ -46,5 +52,41 @@ public class SpoilsCard : PartyCard {
 
 	public int getCarryWeight() {
 		return this.carryWeight;
+	}
+
+	public void setUses(Uses use){
+		this.uses = use;
+	}
+
+	public Uses getUses(){
+		return this.uses;
+	}
+
+	public void setRestrictions(params Restrictions[] restrictions){
+		foreach(Restrictions curRestriction in restrictions) {
+			this.restrictions.Add(curRestriction);
+		}
+	}
+
+	public void setRestrictions(List<Restrictions> restrictions){
+		this.restrictions = restrictions;
+	}
+
+	public List<Restrictions> getRestrictions(){
+		return this.restrictions;
+	}
+
+	public void setPassiveGains(params Gains[] gains){
+		foreach(Gains gain in gains) {
+			passiveGains.Add(gain);
+		}
+	}
+
+	public void setPassiveGains(List<Gains> gains){
+		this.passiveGains = gains;
+	}
+
+	public List<Gains> getPassiveGains(){
+		return this.passiveGains;
 	}
 }
