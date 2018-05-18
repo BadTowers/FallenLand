9d5405beadaaa9ba5db50e3c5e573e0cd9fd1455
JSB <kyle-naber@uiowa.edu>
2018-05-18 16:12:43 -0500

    Renamed factions to faction and treating it like a class rather than a blob of information (similar to how spoils cards work)

---
+++ Assets/Scripts/GameScripts/Faction.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+
+public class Faction {
+
+	//A faction has a name
+	private string facName;
+	//A faction has perks
+	private List<Perk> perks;
+	//A faction has lore
+	private string facLore;
+	//A faction has an ID
+	private int ID;
+
+
+
+	//Constructor
+	public Faction(string name){
+		this.facName = name;
+		perks = new List<Perk>();
+	}
+
+
+	public void setName(string n){
+		this.facName = n;
+	}
+
+	public string getName(){
+		return this.facName;
+	}
+
+	public void setPerks(List<Perk> p){
+		this.perks = p;
+	}
+
+	public void addPerk(Perk p){
+		this.perks.Add(p);
+	}
+
+	public List<Perk> getPerks(){
+		return this.perks;
+	}
+
+	public void setLore(string lore){
+		this.facLore = lore;
+	}
+
+	public string getLore(){
+		return this.facLore;
+	}
+
+	public void setID(int id){
+		this.ID = id;
+	}
+
+	public int getID(){
+		return this.ID;
+	}
+
+}
