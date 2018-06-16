45264af7baf5338e2cac332ddcb82e3485294303
= <kyle-naber@uiowa.edu>
2018-06-16 14:12:26 -0500

    Character cards now have functions to attach and remove spoils cards from them

--- Assets/Scripts/GameScripts/Cards/CharacterCard.cs
+++ Assets/Scripts/GameScripts/Cards/CharacterCard.cs
@@ -7,12 +7,14 @@ public class CharacterCard : PartyCard {
 	private int hp; //The max hp the character has
 	private int psychResistance; //The psych resistance the character has
 	private int carryCapacity; //The carry capacity that a character has
-	//link. This would map some spoils card to some bonuses. ex) any two wheeled vehicle -> +1 movement and +6 carrying capacity TODO
-	//specialAbilities. What bonuses the character card gets. TDC cost 3 less. Auto pass certain encounters. etc TODO
+                               //link. This would map some spoils card to some bonuses. ex) any two wheeled vehicle -> +1 movement and +6 carrying capacity TODO
+                               //specialAbilities. What bonuses the character card gets. TDC cost 3 less. Auto pass certain encounters. etc TODO
+    private List<SpoilsCard> equippedSpoils; //All spoils attached to the character
 
 	public CharacterCard(string title) : base(title) {
+        equippedSpoils = new List<SpoilsCard>();
 
-	}
+    }
 
 	public void setHP(int hp) {
 		this.hp = hp;
@@ -37,5 +39,34 @@ public class CharacterCard : PartyCard {
 	public int getCarryCapacity() {
 		return this.carryCapacity;
 	}
+    
+    //Returns true if it can attach a card (no restrictions stopped it)
+    //Returns false if the card cannot be attached (some restriction stopped it)
+    //TODO maybe at some point, give access to an object that gives the reasons why a card wasn't attached
+    public bool attachSpoilsCard(SpoilsCard toAttach) {
+        bool canBeAttached = true;
+
+        //TODO Check restrictions here
+
+        if (canBeAttached) {
+            this.equippedSpoils.Add(toAttach); //Equip the spoils
+            //TODO: Remove the card from the player's hand
+        }
+
+        return canBeAttached;
+    }
+
+    public bool removeSpoilsCard(SpoilsCard toRemove) {
+        bool canBeRemoved = true;
+
+        //TODO check restrictions here //This may not be necessary for removal, have to check this
+
+        if (canBeRemoved) {
+            //TODO: Remove the card from this character.
+            //TODO: Add the card back to the player
+        }
+
+        return canBeRemoved;
+    }
 }
 
