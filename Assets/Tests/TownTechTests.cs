2ef97be0c1c14d0a62463a389ec8b2c93995b32a
Kyle J. Naber <kylenaber@gmail.com>
2020-05-05 18:17:39 -0500

    A few more town tech tests for the night

--- Assets/Tests/TownTechTests.cs
+++ Assets/Tests/TownTechTests.cs
@@ -12,10 +12,19 @@ namespace Tests
 		public IEnumerator TestGetterSetters()
 		{
 			TownTech townTech = new TownTech("Tech Name");
+			Assert.AreEqual("Tech Name", townTech.GetTechName());
+			townTech.SetTechName("New Tech Name");
+			Assert.AreEqual("New Tech Name", townTech.GetTechName());
+
+			townTech.SetId(10);
+			Assert.AreEqual(10, townTech.GetId());
+
+			townTech.SetIsStartingTech(true);
+			Assert.IsTrue(townTech.GetIsStartingTech());
+			townTech.SetIsStartingTech(false);
+			Assert.IsFalse(townTech.GetIsStartingTech());
 
 			yield return null; //Wait for one frame
-
-			Assert.AreEqual("Tech Name", townTech.GetTechName());
 		}
 	}
 }
