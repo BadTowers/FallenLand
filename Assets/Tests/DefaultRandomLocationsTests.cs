44dbed086ebeaf05c01c3bb811e90145940353d5
Kyle J. Naber <kylenaber@yahoo.com>
2020-05-08 14:50:08 -0500

    Default rand location test added

---
+++ Assets/Tests/DefaultRandomLocationsTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace Tests
+{
+	public class DefaultRandomLocationsTests
+	{
+		[UnityTest]
+		public IEnumerator TestSpoilsCardConstructors()
+		{
+			for (int i = 1; i <= 100; i++)
+			{
+				Assert.IsNotNull(DefaultRandomNumberLocations.RAND_NUM_LOCATIONS[i]);
+			}
+
+			Assert.IsNotNull(DefaultRandomNumberLocations.RAND_NUM_LOCATIONS[-1]);
+
+			yield return null;
+		}
+	}
+}
