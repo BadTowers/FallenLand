e4cbd02835f620be1740597df2c1bfe6fba52655
Kyle J. Naber <kylenaber@yahoo.com>
2020-05-24 15:24:13 -0500

    Added card drag handler file

---
+++ Assets/Scripts/GameScripts/CardDragHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = Vector3.zero;
+    }
+}
