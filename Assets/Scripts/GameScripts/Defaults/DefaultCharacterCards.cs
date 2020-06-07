3099ba894a536999c8e95ee65c80875e16f7b189
Kyle J. Naber <kylenaber@yahoo.com>
2020-06-07 14:42:17 -0500

    Cleaned up compile problems in default characters file

--- Assets/Scripts/GameScripts/Defaults/DefaultCharacterCards.cs
+++ Assets/Scripts/GameScripts/Defaults/DefaultCharacterCards.cs
@@ -38,17 +38,23 @@ namespace FallenLand
 				{Skills.Medical, 6}
 			});
 			link = new ConditionalGain();
-			rewardChoice = new Dictionary<Gains, int>();
-			rewardChoice.Add(Gains.Gain_Survival, 1);
-			rewardChoice.Add(Gains.Gain_Combat, 2);
-			link.AddRewardChoice(rewardChoice);
+            rewardChoice = new Dictionary<Gains, int>
+            {
+                { Gains.Gain_Survival, 1 },
+                { Gains.Gain_Combat, 2 }
+            };
+            link.AddRewardChoice(rewardChoice);
 			link.AddWhenRewardCanBeGained(new List<Times>() { Times.Axe_Equipped, Times.Compass_and_Maps_Equipped });
 			link.SetNumberOfTimesThisRewardCanBeClaimed(Uses.Unlimited);
 			curCard.SetLink(link);
 			conditionalGain = new ConditionalGain();
 			conditionalGain.SetWhenRewardCanBeGained(new List<Times>() { Times.After_Ally_Card_Is_Gained });
 			conditionalGain.SetNumberOfTimesThisRewardCanBeClaimed(Uses.Unlimited);
-			conditionalGain.AddReward(Gains.Gain_Character_Cards_May_Be_Assigned_To_Party, 1);
+            rewardChoice = new Dictionary<Gains, int>
+            {
+                { Gains.Gain_Character_Cards_May_Be_Assigned_To_Party, 1 }
+            };
+            conditionalGain.AddRewardChoice(rewardChoice);
 			curCard.AddConditionalGain(conditionalGain);
 			curCard.SetId(curID);
 			curID++;
