452e243dcdb7c348969465ade53c753339f57696
Kyle J. Naber <kylenaber@yahoo.com>
2020-06-10 22:09:30 -0500

    Renamed State for use in example

---
+++ Assets/Scripts/GameScripts/States/PhaseStates/DuringDealPhase.cs
@@ -0,0 +1,11 @@
+﻿
+namespace FallenLand
+{
+    public class DuringDealPhase : PhaseState
+    {
+        public override bool IsStateOccurring(GameManager gameManager)
+        {
+            return (gameManager.GetCurrentPhase() == Phases.Town_Business_Deal);
+        }
+    }
+}
