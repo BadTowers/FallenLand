7074249506ae3ebf22ca3fb265ab0986ab202705
Kyle J. Naber <kylenaber@yahoo.com>
2020-06-10 22:02:41 -0500

    Added some abstract and concrete states

---
+++ Assets/Scripts/GameScripts/States/PhaseStates/DuringAuctionHousePhase.cs
@@ -0,0 +1,11 @@
+﻿
+namespace FallenLand
+{
+    public class DuringAuctionHousePhase : PhaseState
+    {
+        public override bool IsStateOccurring(GameManager gameManager)
+        {
+            return (gameManager.GetCurrentPhase() == Phases.After_Town_Business_Auction_House);
+        }
+    }
+}
