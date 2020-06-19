43b2c32ae1ebf5abc9c7cebe8a9368e0fa980a08
Kyle J. Naber <kylenaber@yahoo.com>
2020-06-18 21:53:37 -0500

    New constants for sending party information through network

--- Assets/Scripts/GameScripts/Constants.cs
+++ Assets/Scripts/GameScripts/Constants.cs
@@ -19,15 +19,29 @@ namespace FallenLand
 				{ Skills.Medical, 0 }
 			};
 
-		//Used to mark what card is being sent
+		//Used for EvDealCard
 		public const byte SPOILS_CARD = 1;
 		public const byte CHARACTER_CARD = 2;
 		public const byte ACTION_CARD = 3;
+
+		//Used for EvSendPlayerInformation
+		public const byte REMOVE_FROM_TOWN_ROSTER = 1;
+		public const byte REMOVE_FROM_AUCTION_HOUSE = 2;
+		public const byte ADD_CHARACTER_TO_SLOT = 3;
+		public const byte REMOVE_CHARACTER_FROM_SLOT = 4;
+		public const byte ADD_VEHICLE = 5;
+		public const byte REMOVE_VEHICLE = 6;
+		public const byte ADD_SPOILS_TO_SLOT = 7;
+		public const byte REMOVE_SPOILS_FROM_SLOT = 8;
+		public const byte ADD_SPOILS_TO_VEHICLE = 9;
+		public const byte REMOVE_SPOILS_FROM_VEHICLE = 10;
 
 		//Used for both callbacks and for event code
 		public const byte EvMove = 1;
 		public const byte EvFinalMove = 2;
 		public const byte EvDealCard = 3;
 		public const byte EvSendFactionInformation = 4;
+		public const byte EvSendPlayerInformation = 5;
+		public const byte EvRequestUpdateToPlayerInformation = 6;
 	}
 }
