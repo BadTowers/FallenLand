02fd21a8f3a442b17c20958f0e6e0e3097e502d2
Kyle J. Naber <kylenaber@yahoo.com>
2020-11-27 17:02:31 -0600

    Added tests for game world coordinates

---
+++ Assets/Tests/GameWorldCoordinatesTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using FallenLand;
+
+namespace Tests
+{
+	public class GameWorldCoordinatesCoordinatesTests
+	{
+		private GameWorldCoordinates GameWorldCoords;
+
+		[SetUp]
+		public void Setup()
+		{
+			GameWorldCoords = new GameWorldCoordinates(25.432f, 10.32f);
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			GameWorldCoords = null;
+		}
+
+		[UnityTest]
+		public IEnumerator TestGameWorldsConstructorAndGetters()
+		{
+			Assert.AreEqual(25.432f, GameWorldCoords.GetX());
+			Assert.AreEqual(10.32f, GameWorldCoords.GetY());
+
+			yield return null;
+		}
+	}
+}
