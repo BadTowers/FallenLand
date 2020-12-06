63bb6fd644d053358eecad21349acef701c250b4
Kyle J. Naber <kylenaber@yahoo.com>
2020-12-05 22:26:52 -0600

    Added a dice class

---
+++ Assets/Scripts/GameScripts/Dice/Dice.cs
@@ -0,0 +1,18 @@
+﻿
+namespace FallenLand
+{
+	public class Dice
+	{
+        readonly System.Random RandomGen;
+
+		public Dice()
+		{
+			RandomGen = new System.Random();
+		}
+
+		public int RollDice(int numberOfSides)
+		{
+			return RandomGen.Next(1, numberOfSides + 1);
+		}
+	}
+}
