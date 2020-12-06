21e85c5d57f95d295cadaec044d347a32431cd1e
Kyle J. Naber <kylenaber@yahoo.com>
2020-12-05 22:27:36 -0600

    Added mission and piece manager

---
+++ Assets/Scripts/GameScripts/MissionManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace FallenLand
+{
+    public class MissionManager
+    {
+        private GameObject MissionPieceManagerGameObject;
+        private MissionPieceManager MissionPieceManagerInst;
+        private List<Coordinates> MissionLocations;
+
+        public MissionManager()
+        {
+            MissionPieceManagerGameObject = new GameObject();
+            MissionPieceManagerGameObject.AddComponent<MissionPieceManager>();
+            MissionPieceManagerInst = MissionPieceManagerGameObject.GetComponent<MissionPieceManager>();
+            MissionLocations = new List<Coordinates>();
+        }
+
+        public void SetMap(MapCreation map)
+        {
+            MissionPieceManagerInst.SetMap(map);
+        }
+
+        public void AddMissionLocation(int missionNumber, int randomLocationNumber)
+        {
+            Coordinates coord = DefaultRandomNumberLocations.RAND_NUM_LOCATIONS[randomLocationNumber];
+            MissionLocations.Add(coord);
+            MissionPieceManagerInst.CreatePiece(missionNumber, coord);
+        }
+    }
+}
---
+++ Assets/Scripts/GameScripts/MissionPieceManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace FallenLand
+{
+    public class MissionPieceManager : MonoBehaviour
+    {
+        private List<GameObject> MissionPieces;
+        private MapCreation Map;
+        private const float X_OFFSET = 0.04f;
+        private const float Y_OFFSET = 0f;
+
+        public MissionPieceManager()
+        {
+            MissionPieces = new List<GameObject>();
+        }
+
+        public void CreatePiece(int missionNum, Coordinates coords)
+        {
+            if (Map != null)
+            {
+                string pieceName = "MissionPiece" + missionNum.ToString();
+                Debug.Log("Mission piece name is " + pieceName);
+                GameObject missionPrefab = (GameObject)Resources.Load("Prefabs/" + pieceName, typeof(GameObject));
+
+                GameWorldCoordinates gameCoords = Map.GetGameLocationFromCoordinates(coords);
+                GameObject curPiece = (GameObject)Instantiate(missionPrefab, new Vector3(gameCoords.GetX() + X_OFFSET, MapCreation.HEX_HEIGHT, gameCoords.GetY() + Y_OFFSET), Quaternion.identity);
+                curPiece.transform.Rotate(0, 180, 0);
+                curPiece.name = pieceName;
+                curPiece.isStatic = true;
+
+                MissionPieces.Add(curPiece);
+            }
+            else
+            {
+                Debug.LogError("Map wasn't set for Mission Piece Manager. Cannot create piece!");
+            }
+        }
+
+        public void SetMap(MapCreation map)
+        {
+            Map = map;
+        }
+    }
+}
