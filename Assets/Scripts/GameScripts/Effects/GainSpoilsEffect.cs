38db3dbf9c1d94199e23c872bda5348591f6de4c
Kyle J. Naber <kylenaber@yahoo.com>
2021-01-24 10:50:54 -0600

    Fixed effect showing popup when you didn't gain the effect but other did

--- Assets/Scripts/GameScripts/Effects/GainSpoilsEffect.cs
+++ Assets/Scripts/GameScripts/Effects/GainSpoilsEffect.cs
@@ -10,16 +10,24 @@ namespace FallenLand
 
         public override void OnActivate(GameManager gameManager, int playerIndex)
         {
-            EventManager.ShowGenericPopup("Effect " + GetEffectName() + " has been applied.");
+            int myIndex = gameManager.GetIndexForMyPlayer();
+            if (myIndex == playerIndex)
+            {
+                EventManager.ShowGenericPopup("Effect " + GetEffectName() + " has been applied.");
+            }
         }
 
         public override void OnDeactivate(GameManager gameManager, int playerIndex)
         {
             List<Reward> rewards = GetRewardsWhenEffectEnds();
+            int myIndex = gameManager.GetIndexForMyPlayer();
             for (int i = 0; i < rewards.Count; i++)
             {
                 rewards[i].HandleReward(gameManager, playerIndex);
-                EventManager.ShowGenericPopup("Effect " + GetEffectName() + " has been completed or worn off.");
+                if (myIndex == playerIndex)
+                {
+                    EventManager.ShowGenericPopup("Effect " + GetEffectName() + " has been completed or worn off.");
+                }
             }
         }
     }
--- Assets/Scripts/GameScripts/Effects/LoseBonusMovementEffect.cs
+++ Assets/Scripts/GameScripts/Effects/LoseBonusMovementEffect.cs
@@ -11,20 +11,28 @@ namespace FallenLand
         public override void OnActivate(GameManager gameManager, int playerIndex)
         {
             List<Punishment> punishments = GetPunishmetsWhenEffectStarts();
+            int myIndex = gameManager.GetIndexForMyPlayer();
             for (int i = 0; i < punishments.Count; i++)
             {
                 punishments[i].HandlePunishment(gameManager, playerIndex);
-                EventManager.ShowGenericPopup("Effect " + GetEffectName() + " has been applied.");
+                if (myIndex == playerIndex)
+                {
+                    EventManager.ShowGenericPopup("Effect " + GetEffectName() + " has been applied.");
+                }
             }
         }
 
         public override void OnDeactivate(GameManager gameManager, int playerIndex)
         {
             List<Reward> rewards = GetRewardsWhenEffectEnds();
+            int myIndex = gameManager.GetIndexForMyPlayer();
             for (int i = 0; i < rewards.Count; i++)
             {
                 rewards[i].HandleReward(gameManager, playerIndex);
-                EventManager.ShowGenericPopup("Effect " + GetEffectName() + " has been removed.");
+                if (myIndex == playerIndex)
+                {
+                    EventManager.ShowGenericPopup("Effect " + GetEffectName() + " has been removed.");
+                }
             }
         }
     }
